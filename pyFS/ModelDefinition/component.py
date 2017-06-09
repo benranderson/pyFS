320cd083f12d6546ac759673683f6944a0698052
Stuart Roy <stuart.roy@woodgroup.com>
2017-06-09 12:46:35 +0100

    added SpringCouple to component.py

--- pyFS/ModelDefinition/component.py
+++ pyFS/ModelDefinition/component.py
@@ -5,7 +5,8 @@ class Component:
 
     def __repr__(self):
         return '{0}: {1}'.format(self.__str__, self.number)
-        
+
+
 class Node(Component):
 
     def __init__(self, number, x_coord, y_coord, z_coord, CSYS=0):
@@ -14,6 +15,7 @@ class Node(Component):
         self.y = y_coord
         self.z = z_coord
         self.CSYS = CSYS
+
 
 class BeamElement(Component):
 
@@ -32,3 +34,18 @@ class BeamElement(Component):
         self.type = type
         self.CO = CO
         self.bend_radius = bend_radius
+
+
+class SpringCouple:
+
+    def __init__(self, number, N1, N2, rotation, reference_element,
+                 spring_constant_table, CSYS):
+        Component.__init__(self, number)
+        self.N1 = N1
+        self.N2 = N2
+        self.rotation = rotation
+        self.reference_element = reference_element
+        self.spring_constant_table = spring_constant_table
+        self.CSYS = CSYS
+
+
