c3fd96f92e0b839835a84472521a46f24ce62798
Stuart Roy <stuart.roy@woodgroup.com>
2017-06-09 13:43:36 +0100

    added SpringTable class to component.py

--- pyFS/ModelDefinition/component.py
+++ pyFS/ModelDefinition/component.py
@@ -132,3 +132,17 @@ class Material(Component):
 
         self.temperature_table.append([temperature, alpha, E,
                                        allowable_stress])
+
+
+class SpringTable(Component):
+
+    def __init__(self, number, K1, K2, K3, K4, K5, K6, type, CO):
+        Component.__init__(self, number)
+        self.K1 = K1
+        self.K2 = K2
+        self.K3 = K3
+        self.K4 = K4
+        self.K5 = K5
+        self.K6 = K6
+        self.type = type
+        self.CO = CO
