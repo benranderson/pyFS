aefd546e07b1b1adbd361da69bbd71f24af827d2
Stuart Roy <stuart.roy@woodgroup.com>
2017-06-09 12:43:56 +0100

    Added Node and BeamElement into the component.py file. Given how short each ModelDefinition object class file is it would be neater to combine them all into a single component.py file

--- pyFS/ModelDefinition/component.py
+++ pyFS/ModelDefinition/component.py
@@ -15,3 +15,20 @@ class Node(Component):
         self.z = z_coord
         self.CSYS = CSYS
 
+class BeamElement(Component):
+
+    def __init__(self, number, N1, N2, N3, rotation, geometry, material,
+                 relZ, relY, taper, type, CO, bend_radius):
+        Component.__init__(self, number)
+        self.N1 = N1
+        self.N2 = N2
+        self.N3 = N3
+        self.rotation = rotation
+        self.geometry = geometry
+        self.material = material
+        self.relZ = relZ
+        self.relY = relY
+        self.taper = taper
+        self.type = type
+        self.CO = CO
+        self.bend_radius = bend_radius
