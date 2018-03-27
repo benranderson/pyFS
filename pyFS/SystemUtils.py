af2be33f9532523829dd2fbc096f78d7be27d172
Stuart Roy <stuart.roy@woodgroup.com>
2018-03-27 14:08:17 +0100

    Escaped slashes in registry key strings

--- pyFS/SystemUtils.py
+++ pyFS/SystemUtils.py
@@ -10,7 +10,7 @@ if 'win' in sys.platform:
 def get_FS2000_install_directory():
     try:
         reg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
-        key = OpenKey(reg, 'SOFTWARE\Wow6432Node\FS2000\Setup')
+        key = OpenKey(reg, 'SOFTWARE\\Wow6432Node\\FS2000\\Setup')
         install_directory = EnumValue(key, 0)[1]
         return install_directory
     except:
