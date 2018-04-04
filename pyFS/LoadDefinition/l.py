0731e650d24e3ea0cab9d36e899afa7ed7de1f68
MCCALLUM <Jamie.McCallum@WOODGROUP.COM>
2018-04-04 11:33:11 +0100

    NL class defined for nodal loads in l.py.

--- pyFS/LoadDefinition/l.py
+++ pyFS/LoadDefinition/l.py
@@ -8,3 +8,24 @@ class Load:
 
     def LFormat(self):
         return ''
+
+
+class NL(Load):
+
+    def __init__(self, number, node, x_force, y_force, z_force, x_moment,
+                 y_moment, z_moment, conc_mass):
+        Load.__init__(self, number)
+        self.node = node
+        self.Fx = x_force
+        self.Fy = y_force
+        self.Fz = z_force
+        self.Mx = x_moment
+        self.My = y_moment
+        self.Mz = z_moment
+        self.NMass = conc_mass
+
+    def LFormat(self):
+        return ('NF,' + str(self.node) + ',' + str(self.Fx) + ','
+                + str(self.Fy) + ',' + str(self.Fz) + ',' + str(self.Mx) + ','
+                + str(self.My) + ',' + str(self.Mz) + ',' + str(self.NMass)
+                + '\n')
