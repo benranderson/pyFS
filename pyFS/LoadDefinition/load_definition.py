d5277296af252231a9462cc44456f5570d8fd7d8
MCCALLUM <Jamie.McCallum@WOODGROUP.COM>
2018-04-04 09:07:50 +0100

    load_definition module started.

---
+++ pyFS/LoadDefinition/load_definition.py
@@ -0,0 +1,9 @@
+"""
+This module represents the Load Definintion task in the FS2000 GUI. The aim is
+that this module provides equivalent functionality to the Load Definition task
+through an instance of the LoadDefinition class.
+
+Designing the object heirarchy this way allows multiple LoadDefinitions to be
+defined in a pyFS list and these can be used  with single or multiple Model,
+Analysis of Post-Processing objects in a single pyFS model.
+"""
