aebf92786a7e90e2f96e2833bd1b964794fb996b
Jamie McCallum <jamie.mccallum@woodplc.com>
2018-04-09 14:43:10 +0100

    MDLDescription class created. Initiates as a blank dictionary.

--- pyFS/ModelDefinition/mdl.py
+++ pyFS/ModelDefinition/mdl.py
@@ -272,6 +272,12 @@ class MDLList(list):
             self.append(new_item)
 
 
+class MDLDescription(dict):
+
+    def __init__(self, data=[]):
+        dict.__init__(self, data)
+
+
 class Point:
 
     def __init__(self, x, y, z):
